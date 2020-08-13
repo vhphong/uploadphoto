adc0f0a81bc3d6a08d58cb6d0571fe317f095208
Phong Vo <vhphong>
2020-08-12 23:02:07 -0400

    debugged some code lines

--- index.php
+++ index.php
@@ -3,27 +3,35 @@
 ?>
 
 <?php
-  // If upload button is clicked ...
+  // If upload button was clicked ...
   if (isset($_POST['upload'])) {
-    $filename = $_FILES["choosefile"]["name"];
-    $tempname = $_FILES["choosefile"]["tmp_name"];
-    $folder = "uploadedfiles/".$filename;
-
-    $dbconn = mysqli_connect("localhost", "root", "", "dbfiles");
-
-    // Get all the submitted data from the form
-    $sql = "INSERT INTO upfile (filename) VALUES ('$filename')";
-
-    // Execute query
-    mysqli_query($dbconn, $sql);
-
-    // Now let's move the uploaded file into the folder: uploadedfiles
-    if (move_uploaded_file($tempname, $folder)) {
-      echo "File uploaded successfully";
+    // check if any file was chosen?
+    // if no file was chosen
+    if (empty($_FILES["choosefile"]["name"])){
+      echo("No file was selected to upload");
     }
-    else {
-      die("Failed to upload file");
+    else{ // if a file was chosen
+      $filename = $_FILES["choosefile"]["name"];
+      $tempname = $_FILES["choosefile"]["tmp_name"];
+      $folder = "uploadedfiles/".$filename;
+
+      $dbconn = mysqli_connect("localhost", "root", "", "dbfiles");
+
+      // Get all the submitted data from the form
+      $sql = "INSERT INTO upfile (filename) VALUES ('$filename')";
+
+      // Execute query
+      mysqli_query($dbconn, $sql);
+
+      // Move the uploaded file into the folder: uploadedfiles
+      if (move_uploaded_file($tempname, $folder)) {
+        echo "File uploaded successfully";
+      }
+      else {
+        die("Failed to upload file");
+      }
     }
+
   }
   $result = mysqli_query($dbconn, "SELECT * FROM upfile");
 ?>
@@ -31,7 +39,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Image Upload</title>
+  <title>File Upload</title>
+</head>
+<body>
   <link rel="stylesheet" type= "text/css" href ="style.css"/>
   <div id="content">
 
